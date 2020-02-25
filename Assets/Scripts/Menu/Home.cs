c6b5a392e1e01b5556d729778699a046530751ab
Shankar Raghupathi <shnkr@live.in>
2020-02-25 07:23:00 +0530

    - More test scripts

--- Assets/Scripts/Menu/Home.cs
+++ Assets/Scripts/Menu/Home.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Home : MenuLayout
 {
@@ -30,20 +31,41 @@ public class Home : MenuLayout
     void OnButtonSinglePlayer()
     {
         // Switch to SP scene
+        //ScaleButtons(0.0f);
+        BounceButton(Button_SinglePlayer);
+        
     }
 
     void OnButtonMultiPlayer()
     {
         // Switch to MP scene
+        ScaleButtons(0.0f);
     }
 
     void OnButtonHighscores()
     {
         // Switch to High scores
+        ScaleButtons(0.0f);
     }
 
     void OnButtonExit()
     {
         // Fuck off out of code
+        ScaleButtons(0.0f);
+    }
+    
+
+    // Tweens
+    void BounceButton(Button button)
+    {
+        button.transform.DOPunchScale(Vector3.one * 0.15f, 0.25f, 1);
+    }
+
+    void ScaleButtons(float scaleTo, float duration = 1.0f)
+    {
+        Button_SinglePlayer.transform.DOScale(scaleTo, duration);
+        Button_MultiPlayer.transform.DOScale(scaleTo, duration);
+        Button_Highscores.transform.DOScale(scaleTo, duration);
+        Button_Exit.transform.DOScale(scaleTo, duration);
     }
 }
